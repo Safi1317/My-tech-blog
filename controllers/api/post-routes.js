80e58ecdb921b76b94bc7149709c7cbb486f788d
Safia Ali <ali.swaphiyah@gmail.com>
2023-04-03 08:08:40 -0500

    added view file

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,3 +1,18 @@
 const router = require("express").Router();
 const { Post } = require("../../models");
+
+router.get("/", (req, res) => {
+  Post.findAll({
+    include: [User],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+      res.render("posts", { posts });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
